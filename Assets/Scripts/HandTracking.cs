d7ea1f912ccd21f50cc876f61465eeaf17abb728
Achmad Mahathir Prasanjaya <64342664+achmadmahathirps@users.noreply.github.com>
2022-07-09 18:59:17 +0700

    Added landmarks to unity

---
+++ Assets/Scripts/HandTracking.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandTracking : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public UDPReceive udpReceive;
+    public GameObject[] handPoints;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Retrieve UDP data
+        string data = udpReceive.data;
+
+        // Remove square brackets in the data
+        data = data.Remove(0, 1);
+        data = data.Remove(data.Length - 1, 1);
+
+        // Parse the data
+        string[] points = data.Split(',');
+
+        // Send the data to the hand points
+        for (int i = 0; i < 21; i++)
+        {
+            float x = 7 - float.Parse(points[i * 3]) / 50;
+            float y = float.Parse(points[i * 3 + 1]) / 50;
+            float z = float.Parse(points[i * 3 + 2]) / 50;
+
+            handPoints[i].transform.localPosition = new Vector3(x, y, z);
+        }
+    }
+}
